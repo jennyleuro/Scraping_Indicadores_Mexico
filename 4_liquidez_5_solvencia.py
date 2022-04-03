62e3200b826c51cca0c7fa8d119fb2076ea9fad1
Jenny <je.pao596@gmail.com>
2022-04-03 13:16:30 -0500

    Calculadas liquidez y solvencia, guardadas en archivos .csv

--- 4_liquidez_5_solvencia.py
+++ 4_liquidez_5_solvencia.py
@@ -1,9 +1,9 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import pandas as pd
+from datetime import datetime
 
 # Opciones de navegación
 options = webdriver.ChromeOptions()
@@ -33,17 +33,43 @@ WebDriverWait(driver, 5)\
 
 driver.switch_to.window(driver.window_handles[2])
 
-WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.XPATH, 
-    '//tr[@id="1"]//img')))\
-        .click()
+excel_url = driver.find_element(By.XPATH, '//tr[@id="1"]//a').get_attribute('href')
 
-time.sleep(3)
+archivo_excel = pd.ExcelFile(excel_url)
 
-WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.XPATH, 
-    '//tr[@id="37"]//img')))\
-        .click()
+df = archivo_excel.parse('Hoja2', skiprows=2)
 
-driver.close()
+#Cambiando el indice del data frame
+df = df.set_index('Indicadores del Balance General (millones de pesos corrientes)')
+
+#Eliminando columnas innecesarias
+df = df.drop(['Unnamed: 0'], axis = 1)
+fecha_inicial = datetime.strptime("2005/01/01", "%Y/%m/%d")
+for col in df:
+  if col < fecha_inicial:
+    df = df.drop([col], axis=1)
+
+#Cambiando Columnas por filas
+df = df.transpose()
+
+
+#Data frame con sólo la info necesaria
+df_resumen = df[['Depósitos de exigibilidad inmediata','Depósitos a plazo y títulos de crédito emitidos', 'Activo','Capital contable']]
+
+#Calculando Depósitos
+df_resumen['Depósitos'] = df_resumen['Depósitos de exigibilidad inmediata'] + df_resumen['Depósitos a plazo y títulos de crédito emitidos']
+
+#Data frame liquidez calculada
+df_liquidez = pd.DataFrame()
+df_liquidez['Liquidez'] = df_resumen['Depósitos']/df_resumen['Activo']
+
+#Data frame solvencia calculada
+df_solvencia = pd.DataFrame()
+df_solvencia['Solvencia'] = df_resumen['Capital contable']/df_resumen['Activo']
+
+df_solvencia.to_csv('data_solvencia.csv')
+df_liquidez.to_csv('data_liquidez.csv')
+
+print('Se guardaron los archivos')
+
 
--- excel_test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-path = "SH_BM_202112.xlsm"
-
-df = pd.read_excel(path, sheet_name="Hoja2")
-
-fila_113 = df.iloc[1]
-
-print(fila_113)
